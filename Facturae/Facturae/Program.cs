c5c97eab85c5e5ee032766cb9c63a48223914c40
carlosga <carlosga05@gmail.com>
2014-12-05 18:19:20 +0100

    · Added README.markdown file · Updated license headers

--- Facturae/Facturae/Program.cs
+++ Facturae/Facturae/Program.cs
@@ -1,6 +1,6 @@
 /* nFacturae - The MIT License (MIT)
  * 
- * Copyright (c) 2013-2014 Carlos Guzmán Álvarez
+ * Copyright (c) 2012-2014 Carlos Guzmán Álvarez
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
