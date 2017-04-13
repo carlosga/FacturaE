f320cf6e82f64fcf2b287ba8c518cd221714866c
carlosga <carlos.guzman.1@outlook.es>
2017-04-13 10:11:30 +0200

    · Decode ASN integers using BIgInteger

--- src/facturae/RFC2253/ASN1/AsnInteger.cs
+++ src/facturae/RFC2253/ASN1/AsnInteger.cs
@@ -1,18 +1,17 @@
 ﻿// Copyright (c) Carlos Guzmán Álvarez. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Numerics;
+
 namespace ASN1
 {
     public sealed class AsnInteger
-        : AsnValueObject<int>
+        : AsnValueObject<BigInteger>
     {
         public AsnInteger(AsnIdentifier id, byte[] buffer)
             : base(id, buffer)
         {
-            for (int i = 0; i < buffer.Length; i++)
-            {
-                Value = (Value << 8) | (buffer[i] & 0xFF);
-            }
+            Value = new BigInteger(buffer);
         }
     }
 }
